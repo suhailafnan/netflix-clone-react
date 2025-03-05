480e60341bfb2c82aec31815cd0393c5ccd967bf
suhail afnan <suhailafnan00@gmail.com>
2025-03-05 14:49:52 +0530

    horizontaly scrolling of the card in the home page done

--- src/componets/TitleCards/TitleCards.jsx
+++ src/componets/TitleCards/TitleCards.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
-
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
-  return (
-    <div className='titleCards'>
-      <h2>Popular On Netflix</h2>
-    </div>
-  )
-}
+  const cardsRef = useRef();
 
-export default TitleCards
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="title-Cards">
+      <h2>Popular On Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
