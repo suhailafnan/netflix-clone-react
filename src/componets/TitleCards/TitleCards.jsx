fff1cf1cfb0802010dde57f6bf1fb18ed6dbc866
suhail afnan <suhailafnan00@gmail.com>
2025-03-05 14:03:51 +0530

    home page banner and the buttons styles done

--- src/componets/TitleCards/TitleCards.jsx
+++ src/componets/TitleCards/TitleCards.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+
+
 const TitleCards = () => {
   return (
     <div className='titleCards'>
-      
+      <h2>Popular On Netflix</h2>
     </div>
   )
 }
