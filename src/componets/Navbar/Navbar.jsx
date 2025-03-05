303dd8ca9e6f98c161a0a6797df6e95548e9c31e
suhail afnan <suhailafnan00@gmail.com>
2025-03-05 09:33:21 +0530

    this is the first commit in this repo

---
+++ src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Navbar.css";
+import Logo from "../../assets/logo.png";
+import Search_icon from "../../assets/search_icon.svg";
+import Bell_icon from "../../assets/bell_icon.svg";
+import Profile_icon from "../../assets/profile_img.png";
+import Caret_icon from "../../assets/caret_icon.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src={Logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browser By Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={Search_icon} alt="" className="icons" />
+        <p>Children</p>
+        <img src={Bell_icon} alt="" className="icons" />
+        <div className="navbar-profile">
+          <img src={Profile_icon} alt="" className="profile" />
+          <img src={Caret_icon} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../componets/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
